07216559f050eb2437186061c030f8f1db87d3ea
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-10-29 23:56:23 +0300

    add new modal to app.js

--- Belorus/js/app.js
+++ Belorus/js/app.js
@@ -57,11 +57,4 @@ window.onclick = function (event) {
 }
 
 // Get the modal
-let modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
